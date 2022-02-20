22467dbcd34928c132c2762ba548403124999570
João Vitor <j.soares@aparkado.de>
2022-02-20 03:26:17 +0100

    add base app

---
+++ lib/src/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Spotify UI',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
